0a505f6125a89bf845e2996d0e92359ee1a15049
Mihnea-Bogdan ZAROJANU <mihnea.zarojanu@stud.acs.upb.ro>
2026-04-27 13:42:40 +0300

    Add this to git too

---
+++ get_dicts.py
@@ -0,0 +1,107 @@
+import subprocess
+import sys
+import os
+
+HOMEPATH = os.path.dirname(os.path.realpath(__file__))
+
+try:
+    SAVED_OPTIONS = open(f'{HOMEPATH}/.opts').read()
+except:
+    SAVED_OPTIONS = 'e'
+
+HELP = """Command-line arguments:
+'-h' or  '--help': print this help page.
+'-d' or '--debug': print debug info without opening links or saving options.
+"""
+
+EXPLAIN = """Search in english/romanian dictionaries.
+Syntax: '{search query} [-options] [--option_name[ --option_name]]'.
+Search query can have spaces.
+
+The options are: 
+e   (--e[nglish]): search in english dictionaries (default).
+r  (--r[omanian]): search in romanian dictionaries.
+t (--t[ranslate]): translate (reverso -> will choose language based on query words).
+i (--i[ncognito]): search in incognito.
+k      (--k[eep]): keep previous search options.
+
+Last used options will be reused if none are provided."""
+
+ENGLISH = [
+    "https://www.thesaurus.com/browse/{query}",
+    "https://www.merriam-webster.com/thesaurus/{query}",
+    "https://www.wordhippo.com/what-is/another-word-for/{query}.html"
+]
+ROMANIAN = [
+    "https://dexonline.ro/definitie/{query}", 
+    "https://www.dictionardesinonime.ro/?c={query}"
+]
+REVERSO = "https://context.reverso.net/traducere/engleza-romana/{query}"
+
+OPTIONS = ["english", "romanian", "translate", "incognito"]
+
+def main():
+    debug = ('-d' in sys.argv or '--debug' in sys.argv)
+    help  = ('-h' in sys.argv or '--help' in sys.argv)
+
+    if help: 
+        print(HELP)
+        print(EXPLAIN)
+        return
+
+    if not debug and 2 <= len(sys.argv):
+        search = " ".join(sys.argv[1:])
+    else:
+        print(EXPLAIN)
+        saved_options = ", ".join([f'--{option}' for option in OPTIONS if option[0] in SAVED_OPTIONS])
+        search = input(f"\nSaved options: {saved_options}\nSearch for:\n")
+
+    full_name_options = ''
+    while '--' in search:
+        end_pos = search.find('--') + 2
+        while (end_pos < len(search) and search[end_pos] != ' '):
+            end_pos += 1
+        option = search[search.find('--') + 2:end_pos]
+        search = search[:search.find('--')] + search[search.find('--') + 2 + len(option):]
+        full_name_options += option[0]
+
+    options = ''
+    opt_pos = search.rfind('-')
+    if 2 <= opt_pos and search[opt_pos - 1] == ' ':
+        options = search[opt_pos + 1:]
+        search  = search[:opt_pos - 1]
+
+    else:
+        if not full_name_options:
+            options = SAVED_OPTIONS
+
+    options += full_name_options
+    if 'e' not in options and 'r' not in options and 't' not in options:
+        options += 'e'
+
+    # debug
+    if debug:
+        print(f"Args:        {sys.argv}")
+        print(f"Search:      {search}")
+        print(f"All options: {options}")
+        print(f"Full-name:   {full_name_options}")
+        return
+
+    dicts  = []
+    dicts += ENGLISH   if 'e' in options else []
+    dicts += ROMANIAN  if 'r' in options else []
+    dicts += [REVERSO] if 't' in options else []
+
+    chrome_arg = '-incognito' if 'i' in options else ''
+    if search and not search.isspace():
+        for link in dicts:
+            link = link.replace("{query}", search)
+            subprocess.Popen(f'start chrome {chrome_arg} /new-tab \"{link}\"', shell=True)
+    else:
+        print("Empty search query, skipping links.")
+
+    if 'k' not in options:
+        open(f'{HOMEPATH}/.opts', 'w').write(options)
+
+if __name__ == "__main__":
+    main()
